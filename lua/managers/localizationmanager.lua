11cde54de7733183deb1a0624c6e030ddb642f1e
0lafe <0lafethehorsescrub@gmail.com>
2024-11-02 01:27:06 -0500

    fix localization error

--- lua/managers/localizationmanager.lua
+++ lua/managers/localizationmanager.lua
@@ -3,9 +3,8 @@ Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInitRoguelike",
   local language_tbl = {
     [("english"):key()] = "en.txt"
   }
-  local mod_path = "mods/roguelike/"
   local language = language_tbl[SystemInfo:language():key()] or "en.txt"
-  local path = mod_path .. "loc/" .. language
+  local path = Roguelike._mod_path .. "loc/" .. language
   path = io.file_is_readable(path) and path or mod_path .. "loc/en.txt"
 
   loc:load_localization_file(path)
