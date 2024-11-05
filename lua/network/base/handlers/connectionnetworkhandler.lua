6d648ef765b563e167267e0dcc3b785377237f3b
0lafe <0lafethehorsescrub@gmail.com>
2024-11-04 23:17:34 -0600

    WiP

--- lua/network/base/handlers/connectionnetworkhandler.lua
+++ lua/network/base/handlers/connectionnetworkhandler.lua
@@ -1,4 +1,4 @@
-function ConnectionNetworkHandler:feed_lootdrop(card_left_pc, card_right_pc, drop_name, _, _, _, _, sender)
+function ConnectionNetworkHandler:feed_lootdrop(card_left_pc, card_right_pc, _, _, _, _, _, sender)
   local peer = self._verify_sender(sender)
 
   if not peer then
@@ -8,13 +8,6 @@ function ConnectionNetworkHandler:feed_lootdrop(card_left_pc, card_right_pc, dro
   if not managers.hud then
     return
   end
-
-  local lootdrop_data = {
-    peer,
-    card_left_pc,
-    card_right_pc,
-    drop_name
-  }
 
   managers.hud:make_cards_hud(peer, 10, card_left_pc, card_right_pc)
 end
--- lua/states/ingamelobbymenu.lua
+++ lua/states/ingamelobbymenu.lua
@@ -24,3 +24,42 @@ function IngameLobbyMenuState:set_lootdrop()
     managers.network:session():send_to_peers("feed_lootdrop", card_left_pc, card_right_pc, drop_name)
   end
 end
+
+function IngameLobbyMenuState:make_lootdrop()
+  self._mass_drop_data = nil
+  local mass_drop_class = self:get_mass_drop_class()
+
+  if mass_drop_class then
+    self._mass_drop_data = mass_drop_class:get_mass_drop_data() or {}
+    self._mass_drop_data.ready = false
+  end
+
+  if self._mass_drop_data then
+    local amount_cards = managers.lootdrop:get_amount_mass_drop(self._mass_drop_data)
+
+    managers.hud:make_skirmish_cards_hud(managers.network:session() and managers.network:session():local_peer(),
+      amount_cards)
+
+    if not Global.game_settings.single_player and managers.network:session() then
+      managers.network:session():send_to_peers("make_lootdrop_skirmish", amount_cards)
+    end
+  else
+    local max_pc = managers.experience:level_to_stars()
+    local disable_weapon_mods = not managers.lootdrop:can_drop_weapon_mods() and true or nil
+    local card_left_pc = managers.lootdrop:new_fake_loot_pc(nil, {
+      weapon_mods = disable_weapon_mods
+    })
+    local card_right_pc = managers.lootdrop:new_fake_loot_pc(nil, {
+      weapon_mods = disable_weapon_mods
+    })
+
+    managers.hud:make_cards_hud(managers.network:session() and managers.network:session():local_peer(), max_pc,
+      card_left_pc, card_right_pc)
+
+    if not Global.game_settings.single_player and managers.network:session() then
+      managers.network:session():send_to_peers("feed_lootdrop", card_left_pc, card_right_pc, "xp")
+    end
+  end
+
+  self:_set_lootdrop()
+end
