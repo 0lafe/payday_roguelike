f3db6fc650549465d772dc7754e6cdc383f75eaa
0lafe <0lafethehorsescrub@gmail.com>
2025-04-23 15:47:07 -0500

    removed ability to buy weapon mods

--- lua/managers/menu/blackmarketgui.lua
+++ lua/managers/menu/blackmarketgui.lua
@@ -1,3 +1,12 @@
 -- removes the ability to purchase weapons
 function BlackMarketGui:open_weapon_buy_menu(data)
 end
+
+-- Prevent buying weapon mods
+function BlackMarketGui:purchase_weapon_mod_callback(data)
+end
+
+-- Remove buy weapon mod button
+Hooks:PostHook(BlackMarketGui, "_setup", "remove_weapon_mod_buy_button", function(self, ...)
+  self._btns["wm_buy_mod"] = nil
+end)
