2e92ab0c11fe0d5f244f2c506994546bd5563395
0lafe <0lafethehorsescrub@gmail.com>
2025-04-24 11:54:34 -0500

    weird bug and bump version

--- lua/managers/menumanager.lua
+++ lua/managers/menumanager.lua
@@ -51,7 +51,7 @@ Hooks:PostHook(MenuManager, "init", "init_show_career", function(self, is_start_
   local main_node = nodes and nodes.main
 
   local crimenet_index
-  local social_hud_index
+  local social_hub_index
   local story_missions_index
   for k, v in pairs(main_node._items) do
     if v._parameters and v._parameters.name then
@@ -59,20 +59,22 @@ Hooks:PostHook(MenuManager, "init", "init_show_career", function(self, is_start_
       if name == "crimenet" then
         crimenet_index = k
       elseif name == "social_hub" then
-        social_hud_index = k
+        social_hub_index = k
       elseif name == "story_missions" then
         story_missions_index = k
       end
     end
   end
 
-  local temp_crimenet = main_node._items[crimenet_index]
-  local temp_social_hud = main_node._items[social_hud_index]
-  local temp_story_missions = main_node._items[story_missions_index]
+  if crimenet_index and social_hub_index and story_missions_index then
+    local temp_crimenet = main_node._items[crimenet_index]
+    local temp_social_hud = main_node._items[social_hub_index]
+    local temp_story_missions = main_node._items[story_missions_index]
 
-  main_node._items[crimenet_index] = temp_story_missions
-  main_node._items[social_hud_index] = temp_crimenet
-  main_node._items[story_missions_index] = temp_social_hud
+    main_node._items[crimenet_index] = temp_story_missions
+    main_node._items[social_hub_index] = temp_crimenet
+    main_node._items[story_missions_index] = temp_social_hud
+  end
 
   local node = main_node:item("crimenet_offline")
   if node then
